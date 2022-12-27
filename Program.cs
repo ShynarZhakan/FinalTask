0c70a3d4fbf08604ac4287f14eb777924a41f693
ShynarZhakan <schubblezam@gmail.com>
2022-12-27 22:11:32 +0600

    write methods for program

--- Program.cs
+++ Program.cs
@@ -2,3 +2,26 @@
 // либо равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+void FindElementsFromArray (string[] initialArray, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < initialArray.Length; i++)
+    {
+        if (initialArray[i].Length < 4)
+        {
+            newArray[count] = initialArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
